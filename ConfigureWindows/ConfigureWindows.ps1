b49ccfae8c48fc17fbf3e5afbfa8e1dafd3ef04f
Scott Organ <scottorgan@users.noreply.github.com>
2021-02-12 11:39:57 -0600

    Inital commit

---
+++ ConfigureWindows/ConfigureWindows.ps1
@@ -0,0 +1,77 @@
+﻿#Run PowerShell in x64 context
+If ($ENV:PROCESSOR_ARCHITEW6432 -eq "AMD64") {
+    Try {
+        &"$ENV:WINDIR\SysNative\WindowsPowershell\v1.0\PowerShell.exe" -File $PSCOMMANDPATH
+    }
+    Catch {
+        Throw "Failed to start $PSCOMMANDPATH"
+    }
+    Exit
+}
+
+Function CleanUpAndExit() {
+    Param(
+        [Parameter(Mandatory=$True)][String]$ErrorLevel
+    )
+
+    # Write results to registry for Detection
+    $Key = "HKEY_LOCAL_MACHINE\Software\Mountainburg\AutoPilot\StartMenu"
+    $NOW = Get-Date -Format "MM/dd/yyyy HH:mm"
+
+    If ($ErrorLevel -eq "0") {
+        [microsoft.win32.registry]::SetValue($Key, "Success", $NOW)
+    } else {
+        [microsoft.win32.registry]::SetValue($Key, "Failure", $NOW)
+        [microsoft.win32.registry]::SetValue($Key, "Error Code", $Errorlevel)
+    }
+    
+    # Exit Script with the specified ErrorLevel
+    EXIT $ErrorLevel
+}
+
+Function Remove-UwpApps {
+    
+    $AppsList = "Microsoft.DesktopAppInstaller",
+            "Microsoft.Messaging",
+            "Microsoft.Microsoft3DViewer",
+            "Microsoft.MicrosoftOfficeHub",
+            "Microsoft.MicrosoftSolitaireCollection",
+            "Microsoft.OneConnect",
+            "Microsoft.Print3D",
+            "Microsoft.SkypeApp",
+            "Microsoft.Wallet",
+            "Microsoft.Xbox.TCUI",
+            "Microsoft.XboxApp",
+            "Microsoft.XboxGameOverlay",
+            "Microsoft.XboxIdentityProvider",
+            "Microsoft.XboxSpeechToTextOverlay",
+            "Microsoft.ZuneMusic",
+            "Microsoft.ZuneVideo"
+
+    ForEach ($App in $AppsList) {
+        $PackageFullName = (Get-AppxPackage $App).PackageFullName
+        $ProPackageFullName = (Get-AppxProvisionedPackage -Online | Where {$_.Displayname -eq $App}).PackageName
+ 
+        If ($PackageFullName) {
+            Remove-AppxPackage -Package $PackageFullName
+        }
+ 
+        If ($ProPackageFullName) {
+            Remove-AppxProvisionedPackage -Online -PackageName $ProPackageFullName
+        }
+    }
+}
+
+$Error.Clear()
+
+# Remove the unwanted Windows 10 Apps
+Remove-UwpApps
+
+# Configure the default Windows 10 Start Menu and Taskbar
+Import-StartLayout -LayoutPath "$PSScriptRoot\StartMenuAndTaskbar.xml" -MountPath $env:SystemDrive\
+
+If ($Error.Count -gt 0) {
+    CleanUpAndExit -ErrorLevel 101
+}
+
+CleanUpAndExit -ErrorLevel 0
---
+++ README.md
@@ -0,0 +1,4 @@
+# Customize Windows 10 during AutoPilot
+Powershell script used to remove unwanted UWP Apps and set the default layout of the Start Menu and Taskbar.
+
+Deployed during AutoPilot as a Win32 app using the Microsoft Content Prep Tool available at: https://github.com/microsoft/Microsoft-Win32-Content-Prep-Tool
